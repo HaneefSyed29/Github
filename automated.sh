8c670d777209d77c3bf63047873c6d724648457d
HaneefSyed29 <shaneef2905@gmail.com>
2024-09-11 12:33:28 +0530

    Auto commit on 2024-09-11_12-33-28

--- automated.sh
+++ automated.sh
@@ -1,11 +1,11 @@
 @echo off
 cd "/Users/haneefsyed/Desktop/github/Github"
-set TIMESTAMP=%DATE:/=-%_%TIME::=-%
-set FILENAME=newfile_%TIMESTAMP: =%.txt
-echo This is a new file created at %TIMESTAMP%. > %FILENAME%
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+FILENAME="newfile_$TIMESTAMP.txt"
+echo This is a new file created at $TIMESTAMP. > $FILENAME
 git add .
-set COMMIT_MESSAGE=Auto commit on %TIMESTAMP%
-git commit -m "%COMMIT_MESSAGE%"
+COMMIT_MESSAGE="Auto commit on $TIMESTAMP"
+git commit -m "$COMMIT_MESSAGE"
 git push origin main
-echo Git commit and push completed at %TIMESTAMP%.
+echo "Git commit and push completed at $TIMESTAMP."
 echo
